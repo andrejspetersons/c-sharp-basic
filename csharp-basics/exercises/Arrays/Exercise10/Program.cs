2c71d3c78409d2a8cc587bc69787390d6e6be664
andrejspetersons <127120169+andrejspetersons@users.noreply.github.com>
2024-01-10 17:19:52 +0200

    Exercise10.cs
    Remove whitespace 10.01.24

--- csharp-basics/exercises/Arrays/Exercise10/Program.cs
+++ csharp-basics/exercises/Arrays/Exercise10/Program.cs
@@ -26,6 +26,7 @@
 
             return new int[] { countPositive, sumNegative };
         }
+        
         static void Main(string[] args)
         {
             int[] arrNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15 };
