ffcd7bb02b108e8378c6b136b2b6176e5db2ad5b
andrejspetersons <andrejpeterson1@gmail.com>
2024-01-31 16:54:38 +0200

    Add some fixes 31.01.24

--- csharp-basics/exercises/Collections/Histogram/Program.cs
+++ csharp-basics/exercises/Collections/Histogram/Program.cs
@@ -31,45 +31,18 @@ namespace Histogram
 
             foreach (var item in numbers)
             {
-                if (int.TryParse(item, out int value))
+               if(int.TryParse(item,out int value))
                 {
-                    switch (value)
+                    foreach (var range in histogram.Keys)
                     {
-                        case int n when n >= 0 && n <= 9:
-                            histogram["00-09"] += "*";
+                        int lowerValue = int.Parse(range.Split('-')[0]);
+                        int upperValue = range.Contains('-') ? int.Parse(range.Split('-')[1]) : lowerValue;
+
+                        if(value>=lowerValue&& value <= upperValue)
+                        {
+                            histogram[range] += "*";
                             break;
-                        case int n when n >= 10 && n <= 19:
-                            histogram["10-19"] += "*";
-                            break;
-                        case int n when n >= 20 && n <= 29:
-                            histogram["20-29"] += "*";
-                            break;
-                        case int n when n >= 30 && n <= 39:
-                            histogram["30-39"] += "*";
-                            break;
-                        case int n when n >= 40 && n <= 49:
-                            histogram["40-49"] += "*";
-                            break;
-                        case int n when n >= 50 && n <= 59:
-                            histogram["50-59"] += "*";
-                            break;
-                        case int n when n >= 60 && n <= 69:
-                            histogram["60-69"] += "*";
-                            break;
-                        case int n when n >= 70 && n <= 79:
-                            histogram["70-79"] += "*";
-                            break;
-                        case int n when n >= 80 && n <= 89:
-                            histogram["80-89"] += "*";
-                            break;
-                        case int n when n >= 90 && n <= 99:
-                            histogram["90-99"] += "*";
-                            break;
-                        case int n when n==100:
-                            histogram["100"] += "*";
-                            break;
-                        default:
-                            break;
+                        }
                     }
                 }
 
